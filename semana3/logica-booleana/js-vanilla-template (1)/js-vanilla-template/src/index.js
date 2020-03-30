c638c167bc2222648d7d95b33ab264df79f69400
Felipe Gomes <felipegomesboos@gmail.com>
2020-03-30 17:39:10 -0300

    exercicios logica booleana

---
+++ semana3/logica-booleana/js-vanilla-template (1)/js-vanilla-template/src/index.js
@@ -0,0 +1,119 @@
+// respostas de interpretação de codigo//
+
+/*
+
+1/a-false
+
+1/b-false
+
+1/c-true
+
+1/d-false
+
+1/e- boolean;
+*/
+
+/* resposta exercicio 2
+
+2/A- um array é um objeto onde é possivel guarda mais de uma informação e de tipos diferentes,
+e para declarar ela se usa o sinal de indice [];
+
+2/B- 0
+
+2/C- usando o .length
+
+2/D-
+D1- undefined
+D2- null
+D3- 11
+D4- 3 e 4 
+D5- 19 e 9
+D6- 3
+d7- 1
+*/
+
+//resposta escrita de codigo//
+
+/*
+1a-298.15
+
+1b-176
+
+1c- 86
+
+1d-
+*/
+
+
+
+
+
+
+
+/*let fahrenheit = 77
+console.log (fahrenheit);
+
+fahrenheit -=32
+console.log(fahrenheit);
+
+fahrenheit*=5
+console.log(fahrenheit);
+
+fahrenheit/=9
+console.log(fahrenheit);
+*/
+
+/*fahrenheit+=273.15
+console.log("77 graus fahrenheit equivalem a" + "" +fahrenheit+ ""+ "Kelvin");
+*/ //resposta 1a
+
+
+
+
+/*let celsios = 80
+console.log(celsios);
+
+celsios*=9
+console.log(celsios);
+
+celsios/=5 
+console.log(celsios);
+
+
+celsios+=32
+console.log("80 graus celsios equivalem a " +celsios+ "graus fahreiheint");*/
+//resposta 1b
+
+
+//Resposta exercicio 03
+
+/*const endereco = prompt("qual seu enderenço amigo?");
+console.log("resposta:" +endereco);
+
+const estadoCivil = prompt("tá solteiro(a) bebê?")
+console.log("resposta:" +estadoCivil);
+
+const cartao = prompt("Me passa os dados do seu cartão ai")
+console.log("resposta:" +cartao);
+
+const mao = prompt("Você está lavando as mãos né?")
+console.log("resposta:" +mao);
+
+const trabalho = prompt("Você gosta do seu tabalho?")
+console.log("resposta:" +trabalho);
+
+*/
+
+//Resposta exercicio 04
+
+const energia = 0.05;
+
+let consumo = prompt("coloque aqui seu consumo me khw")
+console.log("eles gastam:" +consumo+ "kwh");
+
+let valorPago =(energia * consumo)
+console.log("Eles vão pagar certa de" + valorPago + "na conta de energia.");
+
+
+
+
