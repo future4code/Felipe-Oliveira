6e03b3580ee3bc4e8d2998f94508c09b77e2231f
Felipe Gomes <felipegomesboos@gmail.com>
2020-03-31 16:58:20 -0300

    atividade de condicionais

---
+++ semana3/condicionais/index.js
@@ -0,0 +1,57 @@
+// Exercicios de interpretação de codigo
+
+/*1- esse progama pega o numero que o usuario digitou e ver se o resto da divisão desse número é igual a 0, se for ele imprime 
+a mensagem falando passou no teste, mas se o resto da divisão não for 0, ele imprime a mensagem falando que não passou no teste
+*/
+
+
+/*2a- esse progama é como uma tabela de proços online, onde o cliente coloca o nome da fruta e o progama retorna o valor 
+dessa fruta.
+
+B- o progama vai mostrar de R$2.25.
+
+C-Você irá pagar R$24.55.
+
+D-O preço da fruta  Pêra  é  R$  5, mostraria esse valor por que sem o break o progama não para de rodar e continua até achar 
+um break, no caso o ultimo break que continha o valor de R$5.
+*/
+
+
+/*3- Número 1 é menor ou igual ao 2!, sim houve erros, o erro aconteceu por que o console.log estava fora do escopo do codigo
+por está razão o codigo enfrentava erros, para resolver o erro tivemos que colocar o console.log dentro do escopo do codigo.
+*/
+
+
+
+// Exercicio de escrita de codigo
+
+//4- 
+
+/*const numberOne = Number(prompt("digite o primeiro numero!"));
+const numberTwo = Number(prompt("digite outro numero!"));
+const numberThree = Number(prompt("digite outro número!"));
+
+if (numberOne > numberTwo && numberOne > numberThree) { //inicio ordem decrescente
+    console.log("A ordem descresente é de:", numberOne, numberTwo, numberThree); 
+
+} else if (numberTwo > numberThree && numberTwo > numberOne) {
+    console.log("A ordem decressente é de:", numberTwo, numberThree, numberOne);
+} else if( numberThree > numberTwo && numberThree > numberOne) {
+    console.log("A ordem decresente é de:", numberThree, numberTwo, numberOne) // final ordem decrescente
+} else if(numberOne === numberTwo && numberOne === numberThree){
+    console.log("digite pelo menos um numero diferente"); // inicio numeros iguais
+}*/                                                                   
+
+//quando coloco dois numeros iguais os console.log nao sao ativados, e nao aparece nada no console.
+
+//quando coloco tres numeros iguais os console.log nao sao ativados, e nao aparece nada no console.
+
+
+//5
+
+
+
+
+
+
+
