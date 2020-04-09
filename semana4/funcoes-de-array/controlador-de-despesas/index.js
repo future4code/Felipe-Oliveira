29c1c00f15a58b3b239ebabbe160156025e0607c
Felipe Gomes <felipegomesboos@gmail.com>
2020-04-09 16:22:34 -0300

    controlador de despesas feito em js

---
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -0,0 +1,11 @@
+// Objeto Controlador de Despesa
+
+arrayDeDespesas = [
+    {valor: document.getElementById("despesas").value, tipo: document.getElementById("tipos-de-despesa").value, 
+    descricao: document.getElementById("descricao").value}
+
+]
+
+const botaoCadrastarDespesas= arrayDeDespesas.map((despesas, index, array) => {
+    return 
+}) 
