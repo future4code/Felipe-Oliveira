2a079c12754377dde5e37fc1c80abdc1f82905bb
Felipe Gomes <felipegomesboos@gmail.com>
2020-06-23 05:04:14 -0300

    exercicios incompleto typeScript

---
+++ semana14/introducao-typescript/build/ex03.js
@@ -0,0 +1,6 @@
+var meuArray = [
+    10, 20, 30, 40, 90
+];
+function retornaObDeArray(totalDeNumeros, meuArray) {
+    totalDeNumeros = meuArray.length;
+}
---
+++ semana14/introducao-typescript/src/ex02.ts
@@ -0,0 +1,11 @@
+function somaNumeros(num01:number, num02:number):void {
+ num01=20,  num02=30
+ console.log(num01 + num02)
+ console.log(num01 - num02)
+ console.log(num01 * num02)
+ if(num01 > num02){
+     console.log(num01)
+ }else{
+     console.log(num02)
+ }
+}
